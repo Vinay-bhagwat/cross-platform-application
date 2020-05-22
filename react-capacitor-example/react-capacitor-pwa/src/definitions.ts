a4d5b20ab5c9a205fcb2404d24c101b8aafcf12c
Vinay Bhagwat <vinay.bhagwat@synerzip.com>
2020-05-22 11:26:53 +0530

    removing packages

--- react-capacitor-example/react-capacitor-pwa/src/definitions.ts
+++
@@ -1,9 +0,0 @@
-declare module "@capacitor/core" {
-  interface PluginRegistry {
-    ReactCapacitorPWAPlugin: ReactCapacitorPWAPluginPlugin;
-  }
-}
-
-export interface ReactCapacitorPWAPluginPlugin {
-  echo(options: { value: string }): Promise<{value: string}>;
-}
